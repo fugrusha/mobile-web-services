73bb479f71ff8176d3e84cc4ec85b31e8e60d6d7
fugrusha <andrew.golowko@gmail.com>
2020-01-26 21:13:48 +0200

    Added jackson-dataformat-xml to consume and produce data in xml format

--- src/main/java/com/udemycourse/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/udemycourse/mobileappws/ui/controller/UserController.java
@@ -6,6 +6,7 @@ import com.udemycourse.mobileappws.ui.model.request.UserDetailsRequestModel;
 import com.udemycourse.mobileappws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +16,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/{id}")
+    @GetMapping(path = "/{id}",
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public UserRest getUser(@PathVariable String id) {
         UserRest returnValue = new UserRest();
 
@@ -24,7 +26,10 @@ public class UserController {
         return returnValue;
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+            )
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
 
         UserRest returnValue = new UserRest();
